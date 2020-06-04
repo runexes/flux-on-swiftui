0f0a9da2ae7008c2c7e8039553be7f923ab62b1a
Aleksandr Slyusarev <runexes@gmail.com>
2020-06-04 15:37:39 +0200

    Implement simple FLUX storage

--- FluxOnSwiftUI/ContentView.swift
+++ FluxOnSwiftUI/ContentView.swift
@@ -7,10 +7,63 @@
 //
 
 import SwiftUI
+import Combine
+
+final class Store<State, Action>: ObservableObject {
+
+    typealias Reducer = (State, Action) -> State
+    private let reducer: Reducer
+    @Published var state: State
+
+    init(initialState: State, reducer: @escaping Reducer) {
+        state = initialState
+        self.reducer = reducer
+    }
+
+    func dispatch(action: Action) {
+        state = reducer(state, action)
+    }
+}
 
 struct ContentView: View {
+
+    enum CounterAction {
+        case increment
+        case decrement
+    }
+
+    @ObservedObject var store = Store<Int, CounterAction>(initialState: 0) {
+        (previousState, action) in
+        switch action {
+            case .increment:
+                return previousState + 1
+            case .decrement:
+                return previousState - 1
+        }
+        
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack(spacing: 30) {
+                Text("\(store.state)")
+                    .font(.largeTitle)
+                HStack {
+                    Button(action: {
+                        self.store.dispatch(action: .increment)
+                    }, label: {
+                        Text("Up")
+                    })
+                    Spacer()
+                    Button(action: {
+                        self.store.dispatch(action: .decrement)
+                    }, label: {
+                        Text("Down")
+                    })
+                }
+                .frame(width: 200)
+            }
+        }
     }
 }
 
