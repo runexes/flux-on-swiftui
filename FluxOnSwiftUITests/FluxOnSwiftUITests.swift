34a7442373c27ec3e81bb59057326772988bc5ed
Aleksandr Slyusarev <runexes@gmail.com>
2020-06-04 13:38:25 +0200

    Initial Commit

---
+++ FluxOnSwiftUITests/FluxOnSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  FluxOnSwiftUITests.swift
+//  FluxOnSwiftUITests
+//
+//  Created by Alex on 04.06.2020.
+//  Copyright © 2020 RunEXEs. All rights reserved.
+//
+
+import XCTest
+@testable import FluxOnSwiftUI
+
+class FluxOnSwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
